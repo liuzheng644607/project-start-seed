0ba1a20d39c67271a031a19986f023b99f9bdbb1
liuzheng644607 <793253892@qq.com>
2020-08-17 14:39:53 +0800

    fix: 1

--- src/client/modules/gobang/core/judge.ts
+++ src/client/modules/gobang/core/judge.ts
@@ -15,52 +15,56 @@ export function isWin(grid: Cell, gobang: GoBang) {
   const colIndex = (currentGridIndex) % boardSize;
   // 在第几行，从 0 行开始
   const rowIndex = Math.floor(currentGridIndex / boardSize);
-  const startColIndex = currentGridIndex - colIndex;
-  const endColIndex = startColIndex + boardSize - 1;
   let winGrids: Cell[] = [];
   let count = 5;
   /**
-   * 横线赢法
+   * 判断是否同色
+   * @param r 行索引
+   * @param c 列索引
+   */
+  const processElement = (r: number, c: number) => {
+    const index = r * boardSize + c;
+    const element = grids[index];
+    if (count > 0 && element.color === grid.color) {
+      winGrids.push(element);
+      count--;
+      return true;
+    } else {
+      return false;
+    }
+  };
+  /**
+   * 横线
    */
   if (count !== 0) {
-    for (let index = currentGridIndex; index <= endColIndex; index++) {
-      const element = grids[index];
-      if (count > 0 && element.color === grid.color) {
-        winGrids.push(element);
-        count--;
+    count = 5;
+    winGrids = [];
+    for (let r = rowIndex, c = colIndex; r >= 0 && c < boardSize; c++) {
+      if (!processElement(r, c)) {
+        break;
       }
     }
-    for (let index = currentGridIndex - 1; index >= startColIndex; index--) {
-      const element = grids[index];
-      if (count > 0 && element.color === grid.color) {
-        winGrids.push(element);
-        count--;
+    for (let r = rowIndex, c = colIndex - 1; r >= 0 && c >= 0; c--) {
+      if (!processElement(r, c)) {
+        break;
       }
     }
   }
 
   /**
-   * 竖线赢法
+   * 竖线
    */
   if (count !== 0) {
     count = 5;
     winGrids = [];
-    for (let index = currentGridIndex; Math.floor(index / boardSize) >= 0; index -= boardSize) {
-      const element = grids[index];
-      if (count > 0 && element.color === grid.color) {
-        winGrids.push(element);
-        count--;
+    for (let r = rowIndex, c = colIndex; c >= 0 && r >= 0; r--) {
+      if (!processElement(r, c)) {
+        break;
       }
     }
-    for (
-      let index = currentGridIndex + boardSize;
-      Math.floor(index / boardSize) < boardSize;
-      index += boardSize
-      ) {
-      const element = grids[index];
-      if (count > 0 && element.color === grid.color) {
-        winGrids.push(element);
-        count--;
+    for (let r = rowIndex + 1, c = colIndex; r < boardSize && c >= 0; r++) {
+      if (!processElement(r, c)) {
+        break;
       }
     }
   }
@@ -72,19 +76,13 @@ export function isWin(grid: Cell, gobang: GoBang) {
     count = 5;
     winGrids = [];
     for (let r = rowIndex, c = colIndex; r >= 0 && c < boardSize; r--, c++) {
-      const index = r * boardSize + c;
-      const element = grids[index];
-      if (count > 0 && element.color === grid.color) {
-        winGrids.push(element);
-        count--;
+      if (!processElement(r, c)) {
+        break;
       }
     }
     for (let r = rowIndex + 1, c = colIndex - 1; c >= 0 && r < boardSize; r++, c--) {
-      const index = r * boardSize + c;
-      const element = grids[index];
-      if (count > 0 && element.color === grid.color) {
-        winGrids.push(element);
-        count--;
+      if (!processElement(r, c)) {
+        break;
       }
     }
   }
@@ -96,29 +94,18 @@ export function isWin(grid: Cell, gobang: GoBang) {
     count = 5;
     winGrids = [];
     for (let r = rowIndex, c = colIndex; r >= 0 && c >= 0; r--, c--) {
-      const index = r * boardSize + c;
-      const element = grids[index];
-      if (count > 0 && element.color === grid.color) {
-        winGrids.push(element);
-        count--;
+      if (!processElement(r, c)) {
+        break;
       }
     }
     for (let r = rowIndex + 1, c = colIndex + 1; c < boardSize && r < boardSize; r++, c++) {
-      const index = r * boardSize + c;
-      const element = grids[index];
-      if (count > 0 && element.color === grid.color) {
-        winGrids.push(element);
-        count--;
+      if (!processElement(r, c)) {
+        break;
       }
     }
   }
 
   if (count === 0) {
-    console.log('win', winGrids);
-    winGrids.forEach((c) => {
-      // highlightPoint(c);
-    });
-
     return {
       win: count === 0,
       winGrids,
